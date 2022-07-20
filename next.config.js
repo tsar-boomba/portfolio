4ed9026d900351d0464cc268c5f3e93a3f2f0e3c
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-07-20 13:40:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,5 +7,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
 	swcMinify: true,
-	assetPrefix: isProd ? '/portfolio/' : ''
+	// assetPrefix: isProd ? '/portfolio/' : ''
 });
