7f28337f7578cafe1707de3b3ec6892b10652eb4
tsar-boomba <tsar-boomba@users.noreply.github.com>
2022-09-19 19:40:28 +0000

    deploy: ecd8029be39ab9e0f7641ead93baa0d392e22215

---
+++ _next/static/onbKW2wAnjkFQHDN1vTfG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d0447323-3938c74bf738cd99.js","static/chunks/445-dbc0f40e6196df90.js","static/chunks/pages/index-a01d6ddac8de576b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
