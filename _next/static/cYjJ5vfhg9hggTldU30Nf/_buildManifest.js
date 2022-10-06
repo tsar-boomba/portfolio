5ea7707e1b426b54e1aa718807ff445cfb78067b
tsar-boomba <tsar-boomba@users.noreply.github.com>
2022-10-06 18:32:25 +0000

    deploy: e536338e46094719f3bbb803d37e8ac3d3bb842e

---
+++ _next/static/cYjJ5vfhg9hggTldU30Nf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d0447323-7e54bd928fb885fe.js","static/chunks/445-dbc0f40e6196df90.js","static/chunks/pages/index-f62adee500c34e3e.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
