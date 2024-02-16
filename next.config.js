7a8515cf38e99c0a056c4abc0384d31f728604df
tsar-boomba <itg.2048@gmail.com>
2024-02-16 01:29:38 -0500

    switch to output: export

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
+	output: 'export',
 	// assetPrefix: isProd ? '/portfolio/' : ''
 });
