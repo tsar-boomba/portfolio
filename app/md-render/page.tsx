01c25479bf7698fcab511b10ba7dca037f3ae89c
tsar-boomba <itg.2048@gmail.com>
2024-09-20 23:30:28 -0400

    compress the query string with deflate

--- app/md-render/page.tsx
+++ app/md-render/page.tsx
@@ -5,6 +5,33 @@ import { modals } from '@mantine/modals';
 import { useEffect, useState } from 'react';
 import { TbCopy } from 'react-icons/tb';
 
+const COMPRESSION: CompressionFormat = 'deflate-raw';
+
+const bytesToB64 = (b: Uint8Array): string => {
+	const binString = Array.from(b, (byte) => String.fromCodePoint(byte)).join('');
+	return btoa(binString);
+};
+
+const base64ToBytes = (base64: string): Uint8Array => {
+	const binString = atob(base64);
+	return new Uint8Array(Array.from(binString, (m) => m.codePointAt(0)!));
+};
+
+const readStreamToArray = async (stream: ReadableStream<Uint8Array>): Promise<Uint8Array> => {
+	const arr = [];
+	const reader = stream.getReader();
+
+	while (1) {
+		const { done, value } = await reader.read();
+
+		if (done) break;
+
+		arr.push(...value!);
+	}
+
+	return new Uint8Array(arr);
+};
+
 const MarkdownRenderer = () => {
 	const [mdPresent, setMdPresent] = useState(true);
 	const [html, setHtml] = useState<TrustedHTML | null>(null);
@@ -17,12 +44,20 @@ const MarkdownRenderer = () => {
 		setMdPresent(hasMd);
 
 		if (hasMd) {
-			const md = decodeURIComponent(query.get('md')!);
 			setTimeout(async () => {
 				const domPurifyPromise = import('dompurify');
 				const markedPromise = import('marked');
 
 				try {
+					const compressedMd = base64ToBytes(decodeURIComponent(query.get('md')!));
+					const md = new TextDecoder().decode(
+						await readStreamToArray(
+							new Blob([compressedMd])
+								.stream()
+								.pipeThrough(new DecompressionStream(COMPRESSION)),
+						),
+					);
+
 					// Lazy load these libraries since they are not needed on the main site
 					const [domPurifyModule, markedModule] = await Promise.all([
 						domPurifyPromise,
@@ -58,7 +93,7 @@ const MarkdownRenderer = () => {
 
 	if (error) {
 		<Center mb={64}>
-			<Text c='red' fw={700}>
+			<Text c='red' fw={700} ta='center'>
 				{error}
 			</Text>
 		</Center>;
@@ -71,7 +106,7 @@ const MarkdownRenderer = () => {
 					<div dangerouslySetInnerHTML={{ __html: html }}></div>
 				</Card>
 				<Button pos='sticky' bottom='1rem' component='a' href='/md-render'>
-					Generate Link
+					New MarkDown
 				</Button>
 			</Stack>
 		);
@@ -92,9 +127,22 @@ const MarkdownRenderer = () => {
 				<Button
 					pos='sticky'
 					bottom='1rem'
+					disabled={!inputMd}
 					leftSection={<TbCopy size={16} />}
-					onClick={() => {
-						const encoded = encodeURIComponent(inputMd);
+					onClick={async () => {
+						// Compress for smaller urls
+						const encoded = encodeURIComponent(
+							bytesToB64(
+								await readStreamToArray(
+									new Blob([new TextEncoder().encode(inputMd)])
+										.stream()
+										.pipeThrough(new CompressionStream(COMPRESSION)),
+								),
+							),
+						);
+
+						console.log(encoded.length);
+
 						navigator.clipboard.writeText(
 							process.env.NODE_ENV === 'development'
 								? `http://localhost:3000/md-render?md=${encoded}`
